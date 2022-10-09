59d629762957bf131e0a575fac7df2d331689f32
michat90 <mrygiel1990@gmail.com>
2022-10-09 14:29:12 +0200

    dodanie statementu If

--- src/Project1.java
+++ src/Project1.java
@@ -1,5 +1,12 @@
 public class Project1 {
     public static void main(String[] args) {
         System.out.println("tutaj bedzie projekt nr 1");
+        int a =2;
+        if (a <= 2){
+            System.out.println("Liczba: " +a );
+        }
+        else {
+            System.out.println("Inna liczba");
+        }
     }
 }
